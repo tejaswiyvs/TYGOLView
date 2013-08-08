aed2e2dc53818aec9ec0015fd67385e68c01d5ae
Tejaswi Yerukalapudi <tejaswi.yvs@gmail.com>
2013-08-08 15:40:50 -0400

    reinit

---
+++ GOLTest/GOLTest/TYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TYAppDelegate.h
+//  GOLTest
+//
+//  Created by Tejaswi on 8/8/13.
+//  Copyright (c) 2013 Tejaswi Yerukalapudi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
